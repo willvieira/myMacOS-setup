9713f7a9a2007a2d4bb041f943813d91eb261722
Willian <willian.vieira@usherbrooke.ca>
2019-12-25 17:45:32 +0100

    fix font source code pro

--- macOS_setup.sh
+++ macOS_setup.sh
@@ -258,7 +258,13 @@ source .personal_info
   # plugins=( [plugins...] zsh-syntax-highlighting)
 
   # install Source Code Pro font
-  brew cask install font-source-code-pro
+  git clone https://github.com/powerline/fonts.git --depth=1
+  # install
+  cd fonts
+  ./install.sh
+  # clean-up a bit
+  cd ..
+  rm -rf fonts
 
 ##
 
